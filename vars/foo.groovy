5f7bbca3c48e532badcb62c91aa029a7aeb8afb5
pranabadinu <38889619+pranabadinu@users.noreply.github.com>
2018-05-06 01:08:47 +0530

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -4,9 +4,15 @@
 //varName is a String variable with default value 'Person'
 def call ( String varName = 'Person' ) {
   echo "Hello. The name of person is, ${varName}."
-  display 'Hindustan' 
+  devLang 'JAVA'
+  scriptLang ( shell, perl )
 }
 
-def display (String country = 'India' ) {
-  echo "Country is ${country}"
+//function with one arg
+def devLang (String lang = 'C' ) {
+  echo "Language is ${lang}"
+}
+//function with more arg
+def scriptLang (String scpt1, String scpt2) {
+  echo "Two Scripts are ${scpt1} and ${scpt2}"
 }
