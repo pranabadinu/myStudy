b94e7f93f806fc50a8051de67a410be7b68802a0
pranabadinu <38889619+pranabadinu@users.noreply.github.com>
2018-05-06 00:52:19 +0530

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -4,4 +4,9 @@
 //varName is a String variable with default value 'Person'
 def call ( String varName = 'Person' ) {
   echo "Hello. The name of person is, ${varName}."
+  display 'INR' 
+}
+
+def display (String country = 'India' ) {
+   echo "Country is India"
 }
