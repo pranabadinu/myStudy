bba76f95f06458c95e36e1062efb0d64b425ee13
pranabadinu <38889619+pranabadinu@users.noreply.github.com>
2018-05-06 01:00:53 +0530

    Update myVariables.groovy

--- vars/myVariables.groovy
+++ vars/myVariables.groovy
@@ -4,10 +4,10 @@
 //varName is a String variable with default value 'Person'
 def call( String varName = 'Person' ) {
   echo "Hello. The name of person is, ${varName}."
-  addition 5 6
+  addition ( 5 , 6 )
 }
 
-def addition (int a, int b) {
+def addition (int a=1, int b=2) {
   echo "Addition is ${a+b}"
 }
 
