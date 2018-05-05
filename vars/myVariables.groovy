3b664225b64e53e92742c098009efa607ac21d2e
pranabadinu <38889619+pranabadinu@users.noreply.github.com>
2018-05-05 23:48:01 +0530

    Update myVariables.groovy

--- vars/myVariables.groovy
+++ vars/myVariables.groovy
@@ -1,4 +1,5 @@
 #!/usr/bin/env groovy
+def varTemp = "Hello"
 //varName is a String variable with default value 'Person'
 def call( String varName = 'Person' ) {
   echo "Hello. The name of person is, ${varName}."
