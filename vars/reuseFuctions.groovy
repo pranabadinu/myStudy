47334e0c57e2b084757286f9397ce87592920ec2
pranabadinu <38889619+pranabadinu@users.noreply.github.com>
2018-05-06 09:51:34 +0530

    Update reuseFuctions.groovy

--- vars/reuseFuctions.groovy
+++ vars/reuseFuctions.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 /*********
 Bedefault call() method is exposed as the main entry to this groovy.
-If another method say- functionA is defined in this groovy then that method need to invoke explicitly like: reuseFunctions.functionA 
-from JenkinsFile stage.
+If another method say- functionA is defined in this groovy then call() method can invoke that functionA method.
 *********/
 def call () {
   echo "the main call method of reuseFunctions"
+  functionA
 }
 
 def functionA() {
