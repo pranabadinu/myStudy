1f05063971ba46088cb8992345daeaf7f78b3673
pranabadinu <38889619+pranabadinu@users.noreply.github.com>
2018-05-06 10:22:41 +0530

    Delete commonMathFunctions.groovy

--- vars/commonMathFunctions.groovy
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env groovy
-
-/* mathematical functions which can be used by Jenkins pipeline 
-*/
-
-def call (String func1= 'add' , String func2= 'sub' ) {
-  echo "The invoked function1 is ${func1} and function2 is ${func2}
-}
