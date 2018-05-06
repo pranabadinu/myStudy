88bbbecbe61fa39f6b1c13de41bba0f0c63bc5ca
pranabadinu <38889619+pranabadinu@users.noreply.github.com>
2018-05-06 10:02:06 +0530

    Create commonMathFunctions.groovy

---
+++ vars/commonMathFunctions.groovy
@@ -0,0 +1,8 @@
+#!/usr/bin/env groovy
+
+/* mathematical functions which can be used by Jenkins pipeline 
+*/
+
+def call (String func1='', String func2='') {
+  echo "The invoked function1 is ${func1} and function2 is ${func2}
+}
